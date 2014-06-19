360d08235e5701c1d3ba8a06b83c242a99e26af0
Jan Maghuyop <jangmaghuyop@gmail.com>
2014-06-19 20:45:40 +0800

    added 2 constructors with simple validations

--- sample-1/Person.java
+++ sample-1/Person.java
@@ -5,8 +5,32 @@ class Person {
 	public String last_name;
 	public int age;
 
-	// constructor
+	// constructor no parameters
 	public Person() {
+	}
+
+	// constructor with 2 parameters
+	public Person(String first_name, String last_name) {
+		if(first_name.equals("John")) {
+			this.first_name = "Jan";
+		} else {
+			this.first_name = first_name;
+		}
+		age = 0;
+	}
+
+	// constructor with 3 parameters with simple age validation
+	public Person(String first_name, String last_name, int age) {
+		this(first_name, last_name);
+		if(age > 0) {
+			if(age > 100) {
+				age = 100;
+			} else {
+				this.age = age;
+			}
+		} else {
+			age = 0;
+		}
 	}
 
 	// methods
